69c2a46acd192fa080875d26b195cf5806f900c2
Jong Ho Yuk <wjswk37510@kookmin.ac.kr>
2018-06-17 02:32:23 +0900

    UpDown.py수정

--- UpDown.py
+++ UpDown.py
@@ -3,6 +3,8 @@ class updown:
     def new(self, limit):
         self.b=random.randint(1,limit)
         self.count=0
+    def answer(self):
+        return self.b
     def start(self,guess_number):
         if  guess_number> self.b:
                 self.count+=1
@@ -10,13 +12,15 @@ class updown:
         if  guess_number< self.b:
                 self.count+=1
                 return "UP"
-        if  guess_number== self.b:
-                return "Success. 총 {}회 걸렸습니다.".format(self.count)
+        if guess_number== self.b:
+                self.count+=1
+                return "Success! 정답을 맞추는데 총 {}회 걸렸습니다.".format(self.count)
 if __name__=="__main__":
     s=updown()
+    guess_number=0
     limit=int(input())
     s.new(limit)
-    while True:
+    while s.answer()!=guess_number:
         guess_number=int(input())
         if limit < guess_number:
             print("Error! 다시 입력하세요")
